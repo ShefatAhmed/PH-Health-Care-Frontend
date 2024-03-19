1e95555a9c7e69232fa16250c64c1bd615fc52a7
unknown <shefata69@gmail.com>
2024-03-19 12:31:58 +0600

    first commit

---
+++ src/app/(withCommonLayout)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Button>Text</Button>
+    </div>
+  );
+};
+
+export default HomePage;
