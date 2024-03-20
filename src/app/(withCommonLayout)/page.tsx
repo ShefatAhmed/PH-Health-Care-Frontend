4010bdb84e7ccd0939ff9d498f2f57a07877f4ea
unknown <shefata69@gmail.com>
2024-03-20 17:40:32 +0600

    Specialties Section

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/UI/Homepage/HeroSection/HeroSection";
+import Specialist from "@/components/UI/Homepage/Specialist/Specialist";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
+      <Specialist />
     </>
   );
 };
