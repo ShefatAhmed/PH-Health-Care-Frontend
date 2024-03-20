34a675f98a3d1b85be84ff5bf5520f97f8b44c85
unknown <shefata69@gmail.com>
2024-03-20 22:17:09 +0600

    top rated doctor

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "@/components/UI/Homepage/HeroSection/HeroSection";
 import Specialist from "@/components/UI/Homepage/Specialist/Specialist";
+import TopRatedDoctors from "@/components/UI/Homepage/TopRatedDoctors/TopRatedDoctors";
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
       <Specialist />
+      <TopRatedDoctors />
     </>
   );
 };
